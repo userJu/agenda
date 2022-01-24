97c458d391509c132bd47fb51f3c735bfdb5c7b8
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-01-24 19:37:39 +0900

    login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { AuthService } from "../service/Auth-service";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
 import { auth } from "../service/fireBase";
 
 const Login = () => {
@@ -19,12 +23,28 @@ const Login = () => {
         const credential = GoogleAuthProvider.credentialFromError(error);
       });
   };
+  const GithubLogin = () => {
+    const provider = new GithubAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = GithubAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
   return (
     <div>
       <h1>AGENDA</h1>
       <div>
         <button onClick={GoogleLogin}>Google</button>
-        <button>Github</button>
+        <button onClick={GithubLogin}>Github</button>
         <button>Kakao</button>
         <button>Naver</button>
       </div>
